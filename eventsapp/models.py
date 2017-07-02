75c88ac9b91ec6c32341ec361d6e29616a345d51
Jonathan Hernandez <jbhernan@gmail.com>
2017-07-02 13:11:30 -0700

    added model and form

--- eventsapp/models.py
+++ eventsapp/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+# Model for a meetup group that am following and for which
+# want to load events. Stores the URL name of the meetup group.
+class MeetupGroup(models.Model):
+  url_name = models.TextField(unique=True)
+  meetup_group_id = models.AutoField(primary_key = True)
+
+
--- eventsapp/views.py
+++ eventsapp/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import MeetupGroup
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
+
+def follow(request):
+  meetupNames = request.POST.getlist('meetupgroup')
+  for name in meetupNames:
+    meetupGroup = MeetupGroup(name)
+    meetupGroup.save()
+  return HttpResponse('Success!')
 
 
 def db(request):
