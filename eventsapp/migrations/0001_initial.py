0267002b54f0d9c5a9c4ef192d9c4972e470f882
Jonathan Hernandez <jbhernan@gmail.com>
2017-07-02 13:30:05 -0700

    migrate

--- eventsapp/migrations/0001_initial.py
+++ eventsapp/migrations/0001_initial.py
@@ -20,4 +20,11 @@ class Migration(migrations.Migration):
                 ('when', models.DateTimeField(auto_now_add=True, verbose_name=b'date created')),
             ],
         ),
+        migrations.CreateModel(
+            name='MeetupGroup',
+            fields=[
+                ('url_name', models.TextField(unique=True, verbose_name='Meetup url name')),
+                ('meetup_group_id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Meetup Group ID')),
+            ],
+        ),
     ]
